e8df54281903a72861d97df56e3e120ad69098db
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2012-09-08 18:08:31 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+🌑 🌒 🌓 🌔 🌕 🌖 🌗 🌘 🌑
+==================
+
+This HTML page and javascript draw an `svg` picture of the moon that tries to match the current 
+actual phase of the moon.
+
+Phase of the moon
+-----------------
+
+http://www.ben-daglish.net/moon.shtml
+
+
+Background image of stars
+----------------
+
+© StarFields [1] 1998 - 2012. All Rights Reserved.
+You are very welcome to use my background images on your website
+or blog free of charge. All other uses, please contact StarFields [2] for
+permission. Please do not hotlink the images, save them to your own
+computer first. Enjoy!
+
+[1] http://starfields.ws/
+[2] http://1-background.com/contact/
+
+
+
+Julian date
+----------
+
+http://stackoverflow.com/questions/11759992/calculating-jdayjulian-day-in-javascript
+http://jsfiddle.net/gkyYJ/
+http://stackoverflow.com/users/965051/adeneo
+
+
---
+++ moon-phase.js
@@ -0,0 +1,86 @@
+// http://stackoverflow.com/questions/11759992/calculating-jdayjulian-day-in-javascript
+// http://jsfiddle.net/gkyYJ/
+// http://stackoverflow.com/users/965051/adeneo
+Date.prototype.getJulian = function() {
+    return Math.floor((this / 86400000) - (this.getTimezoneOffset() / 1440) + 2440587.5);
+};
+
+// http://www.ben-daglish.net/moon.shtml
+function moon_day(today) {
+    var GetFrac = function(fr) {
+        return (fr - Math.floor(fr));
+    };
+    var thisJD = today.getJulian();
+    var year = today.getFullYear();
+    var degToRad = 3.14159265 / 180;
+    var K0, T, T2, T3, J0, F0, M0, M1, B1, oldJ;
+    K0 = Math.floor((year - 1900) * 12.3685);
+    T = (year - 1899.5) / 100;
+    T2 = T * T;
+    T3 = T * T * T;
+    J0 = 2415020 + 29 * K0;
+    F0 = 0.0001178 * T2 - 0.000000155 * T3 + (0.75933 + 0.53058868 * K0) - (0.000837 * T + 0.000335 * T2);
+    M0 = 360 * (GetFrac(K0 * 0.08084821133)) + 359.2242 - 0.0000333 * T2 - 0.00000347 * T3;
+    M1 = 360 * (GetFrac(K0 * 0.07171366128)) + 306.0253 + 0.0107306 * T2 + 0.00001236 * T3;
+    B1 = 360 * (GetFrac(K0 * 0.08519585128)) + 21.2964 - (0.0016528 * T2) - (0.00000239 * T3);
+    var phase = 0;
+    var jday = 0;
+    while (jday < thisJD) {
+        var F = F0 + 1.530588 * phase;
+        var M5 = (M0 + phase * 29.10535608) * degToRad;
+        var M6 = (M1 + phase * 385.81691806) * degToRad;
+        var B6 = (B1 + phase * 390.67050646) * degToRad;
+        F -= 0.4068 * Math.sin(M6) + (0.1734 - 0.000393 * T) * Math.sin(M5);
+        F += 0.0161 * Math.sin(2 * M6) + 0.0104 * Math.sin(2 * B6);
+        F -= 0.0074 * Math.sin(M5 - M6) - 0.0051 * Math.sin(M5 + M6);
+        F += 0.0021 * Math.sin(2 * M5) + 0.0010 * Math.sin(2 * B6 - M6);
+        F += 0.5 / 1440;
+        oldJ = jday;
+        jday = J0 + 28 * phase + Math.floor(F);
+        phase++;
+    }
+
+    // 29.53059 days per lunar month
+    return (((thisJD - oldJ) / 29.53059));
+}
+
+function phase_junk(phase) {
+    var sweep = [];
+    var mag;
+    if (phase <= 0.25) {
+        sweep = [ 1, 0 ];
+        mag = 20 - 20 * phase * 4
+    } else 
+    if (phase <= 0.50) { 
+        sweep = [ 0, 0 ];
+        mag = 20 * (phase - 0.25) * 4
+    } else
+    if (phase <= 0.75) {
+        sweep = [ 1, 1 ];
+        mag = 20 - 20 * (phase - 0.50) * 4
+    } else
+    if (phase <= 1) {
+        sweep = [ 0, 1 ];
+        mag = 20 * (phase - 0.75) * 4
+    } else { 
+        exit; 
+    }
+    // http://www.w3.org/TR/SVG/paths.html#PathDataEllipticalArcCommands
+    var d = "m100,0 ";
+    d = d + "a" + mag + ",20 0 1," + sweep[0] + " 0,150 ";
+    d = d + "a20,20 0 1," + sweep[1] + " 0,-150";
+    // http://www.i-programmer.info/programming/graphics-and-imaging/3254-svg-javascript-and-the-dom.html
+    xmlns = "http://www.w3.org/2000/svg";
+    var svg = document.getElementById("moon");
+    var path = document.createElementNS(xmlns, 'path');
+    var back = document.createElementNS(xmlns, 'path');
+    back.setAttribute('class', 'moonback');
+    back.setAttribute('d', "m100,0 a20,20 0 1,1 0,150 a20,20 0 1,1 0,-150");
+    path.setAttribute('class', 'moon');
+    path.setAttribute('d', d);
+    svg.appendChild(back);
+    svg.appendChild(path);
+    document.getElementsByTagName("body")[0].appendChild(svg);
+}
+
+phase_junk(moon_day(new Date()));
